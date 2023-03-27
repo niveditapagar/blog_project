c031dfae4cc270cd4a1b83765ee93be260b0299d
Nivedita <nivedita.pagar@outlook.com>
2023-03-27 19:37:43 +0200

    Cleanup project files

--- my_blog_site/blog/forms.py
+++ my_blog_site/blog/forms.py
@@ -3,11 +3,9 @@ from blog.models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
         fields = ('author','title', 'text',)
-
         widgets = {
             'title': forms.TextInput(attrs={'class': 'textinputclass'}),
             'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
@@ -15,11 +13,9 @@ class PostForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
-
     class Meta:
         model = Comment
         fields = ('author', 'text',)
-
         widgets = {
             'author': forms.TextInput(attrs={'class': 'textinputclass'}),
             'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
